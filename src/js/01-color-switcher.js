5722e1157ec8b67bf0e7daff8f46baaabf1d25b5
Alina <alinasy245@gmail.com>
2024-01-06 19:28:40 +0200

    Changes

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+// 1 завдання
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const changeBackgroundColor = () => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+};
+
+const onButtonStartClick = () => {
+  refs.btnStart.setAttribute('disabled', '');
+  refs.btnStop.removeAttribute('disabled');
+  timerId = setInterval(changeBackgroundColor, 1000);
+};
+
+const onButtonStopClick = () => {
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.setAttribute('disabled', '');
+  clearInterval(timerId);
+};
+
+refs.btnStart.addEventListener('click', onButtonStartClick);
+refs.btnStop.addEventListener('click', onButtonStopClick);
---
+++ src/js/02-timer.js
@@ -0,0 +1,69 @@
+// 2 завдання
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  timerValues: document.querySelectorAll('.value'),
+  inputTime: document.querySelector('#datetime-picker'),
+};
+
+let targetDate = null;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    if (selectedDates[0] - new Date() <= 0) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      targetDate = null;
+      return;
+    }
+
+    refs.startBtn.removeAttribute('disabled');
+    targetDate = selectedDates[0];
+  },
+};
+
+const convertMs = ms => {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+const loadTimer = () => {
+  const timeCounter = targetDate - new Date();
+  const timer = convertMs(timeCounter);
+  Object.entries(timer).forEach(([key, value], index) => {
+    refs.timerValues[index].textContent = String(value).padStart(2, 0);
+  });
+
+  if (timeCounter <= 1000) clearInterval(timerId);
+};
+
+const disableElement = elem => {
+  elem.setAttribute('disabled', '');
+};
+
+const onStartTimerClick = () => {
+  disableElement(refs.startBtn);
+  disableElement(refs.inputTime);
+  timerId = setInterval(loadTimer, 1000);
+};
+
+refs.startBtn.addEventListener('click', onStartTimerClick);
+
+flatpickr('#datetime-picker', options);
